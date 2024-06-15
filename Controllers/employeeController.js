45bece732cbbca786dc08e932fc366151a56156d
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-15 21:36:20 +0530

    third

--- Controllers/employeeController.js
+++ Controllers/employeeController.js
@@ -90,3 +90,64 @@ export const employeeForgotPassword = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const getAllEmployees = async(req, res) =>{
+    try {
+        const employees=await Employee.find();
+        res.status(200).json({message:"All employees list",result:employees})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const createEmployee=async(req,res)=>{
+    try {
+        const{username,email,password,dob,gender,city,contact,role,degree,department,accountNumber,salary}=req.body;
+        const hashPassword= await bcryptjs.hash(password,10);
+        const count = await Employee.countDocuments();
+        const newEmployee=new Employee({
+            id:count+1,
+            username,
+            email,
+            password:hashPassword,dob,gender,city,contact,role,degree,department,accountNumber,salary
+        });
+        await newEmployee.save();
+        res.status(200).json({message:"Employee created successfully", result:newEmployee})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Employee creation failed"})
+    }
+}
+
+export const updateEmployee=async(req,res)=>{
+    try {
+        const { id } = req.params;
+        const {
+            username, email, password, dob, gender, city, contact, role, degree, department, accountNumber, salary
+        } = req.body;
+        const employee = await Employee.findOne({ id });
+
+        if (!employee) {
+            return res.status(404).json({
+                message: "Employee not found"
+            });
+        }
+        let updateData = {username,email,dob,gender,city,contact,role,degree,department,accountNumber,salary};
+
+        if (password) {
+            updateData.password = await bcryptjs.hash(password, 10);
+        }
+        const updatedEmployee = await Employee.findByIdAndUpdate(employee._id, updateData, { new: true });
+
+        res.status(200).json({
+            message: "Employee updated successfully",
+            result: updatedEmployee
+        });
+    } catch (error) {
+        console.error("Error updating employee:", error);
+        res.status(500).json({
+            message: "Employee update failed"
+        });
+    }
+}
--- Routers/employeeRouter.js
+++ Routers/employeeRouter.js
@@ -1,10 +1,13 @@
 import  express  from "express";
-import { employeeForgotPassword, employeeLogin, employeeReg } from "../Controllers/employeeController.js";
+import { createEmployee, employeeForgotPassword, employeeLogin, employeeReg, getAllEmployees, updateEmployee } from "../Controllers/employeeController.js";
 
 const router1=express.Router();
 
 router1.post("/employeeReg",employeeReg)
 router1.post("/employeeLogin",employeeLogin)
 router1.post("/employeeForgotPassword",employeeForgotPassword)
+router1.get("/getAllEmployees",getAllEmployees)
+router1.post("/createEmployee",createEmployee)
+router1.put("/updateEmployee/:id",updateEmployee)
 
 export default router1;
