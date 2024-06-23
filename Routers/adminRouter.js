3574fefedff86191f866588e4a2830af7fcaa72e
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-23 22:02:28 +0530

    ui

--- Routers/adminRouter.js
+++ Routers/adminRouter.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { adminDashboard, adminForgotPassword, adminLogin, adminReg,  createJobpost, getAllJobs } from "../Controllers/adminController.js";
+import { adminDashboard, adminForgotPassword, adminLogin, adminReg,  createJobpost, getAllAdmin, getAllJobs } from "../Controllers/adminController.js";
 import adminMiddleware from "../Middleware/adminMiddleware.js";
 
 const router=express.Router();
@@ -11,6 +11,8 @@ router.get("/adminDashboard",adminMiddleware,adminDashboard)
 
 router.post("/createJobpost",createJobpost)
 router.get("/getAllJobs",getAllJobs)
+
+router.get("/getAllAdmin",getAllAdmin)
 
 
 export default router;
